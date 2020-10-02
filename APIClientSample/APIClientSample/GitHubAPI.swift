72f0af7dd258ac100931fa2c118ab54b0e5d2feb
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-02 21:40:49 +0900

    GitHubAPIのResponseを扱う汎用的なenumを追加

--- APIClientSample/APIClientSample/GitHubAPI.swift
+++ APIClientSample/APIClientSample/GitHubAPI.swift
@@ -8,6 +8,40 @@
 
 import Foundation
 
+// 型Aか型Bのどちらかのオブジェクトを表す型。
+// たとえば、Either<String, Int>は文字列か整数のどちらかを意味する。
+// なお、慣例的にどちらの型かを左右で表現することが多い。
+enum Either<Left, Right> {
+    /// Eigher<A, B> の A の方の型。
+    case left(Left)
+
+    /// Eigher<A, B> の B の方の型。
+    case right(Right)
+
+
+    // もし、左側の型ならその値を、右側の型ならnilを返す。
+    var left: Left? {
+        switch self {
+        case let .left(x):
+            return x
+
+        case .right:
+            return nil
+        }
+    }
+
+    // もし、右側の型ならその値を、左側の型ならnilを返す。
+    var right: Right? {
+        switch self {
+        case .left:
+            return nil
+
+        case let .right(x):
+            return x
+        }
+    }
+}
+
 // GitHub Zen APIの結果（現在のCodableにあたる？）
 struct GitHubZen {
     let text: String
