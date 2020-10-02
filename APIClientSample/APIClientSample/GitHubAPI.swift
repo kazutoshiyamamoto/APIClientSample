4291cd26dedaf021d97a1deb69b1ab6b3d105f12
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-02 21:47:57 +0900

    Eitherを使って正常系と異常系どちらが帰ってきても対応できるようにする

--- APIClientSample/APIClientSample/GitHubAPI.swift
+++ APIClientSample/APIClientSample/GitHubAPI.swift
@@ -11,31 +11,31 @@ import Foundation
 // 型Aか型Bのどちらかのオブジェクトを表す型。
 // たとえば、Either<String, Int>は文字列か整数のどちらかを意味する。
 // なお、慣例的にどちらの型かを左右で表現することが多い。
+// Result型を使えばここいらなくなるかも？
 enum Either<Left, Right> {
     /// Eigher<A, B> の A の方の型。
     case left(Left)
-
+    
     /// Eigher<A, B> の B の方の型。
     case right(Right)
-
-
+    
     // もし、左側の型ならその値を、右側の型ならnilを返す。
     var left: Left? {
         switch self {
         case let .left(x):
             return x
-
+            
         case .right:
             return nil
         }
     }
-
+    
     // もし、右側の型ならその値を、左側の型ならnilを返す。
     var right: Right? {
         switch self {
         case .left:
             return nil
-
+            
         case let .right(x):
             return x
         }
@@ -46,8 +46,15 @@ enum Either<Left, Right> {
 struct GitHubZen {
     let text: String
     
-    // レスポンスからわかりやすいオブジェクトへ変換する関数
-    static func from(response: Response) -> GitHubZen {
+    // レスポンスからわかりやすいオブジェクトへと変換する関数。
+    // ただし、サーバーがエラーを返してきた場合などは変換できないので、
+    // その場合はエラーを返す。つまり、戻り値はエラーがわかりやすいオブジェクトになる。
+    // このような、「どちらか」を意味する Either という型で表現する。
+    // GitHubZenが左でなく右なのは、正しいとRightをかけた慣例。
+    // Result型を使えばここいらなくなるかも？
+    static func from(response: Response) -> Either<TransformError, GitHubZen> {
+        
+    }
         
     }
 }
